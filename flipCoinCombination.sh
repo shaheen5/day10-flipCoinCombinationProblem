d43e810b9a9dd6f046380f1462604a1aa6020c51
unknown <miyashaheen1995@gmail.com>
2020-11-06 18:37:30 +0530

    Added:functionalities

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -21,3 +21,32 @@ h=`echo ${flipCoin[H]} | awk '{print ($1/50)*100}'`
 echo "Percentage of 'H' : $h%"
 t=`echo ${flipCoin[T]}|awk '{print ($1/50)*100}'`
 echo "Percentage of 'T' : $t% "
+
+
+echo -e "\n***************Doublet Combination********************\n"
+declare -A flipCoin2   #declare dictionary
+#function to count coin flips
+function doublet_getCount(){
+case $1 in
+	0)flipCoin2[HH]=$((flipCoin2[HH]+1));;
+	1)flipCoin2[HT]=$((flipCoin2[HT]+1));;
+	2)flipCoin2[TH]=$((flipCoin2[TH]+1));;
+	3)flipCoin2[TT]=$((flipCoin2[TT]+1));;
+esac
+}
+count1=0
+while [ $count1 -lt 50 ]
+do
+	doublet_getCount $((RANDOM%4))
+	((count1++))
+done
+echo "Doublet Combination : "${!flipCoin2[@]}
+echo "No. ofOccurrences   : "${flipCoin2[@]}
+hh=`echo ${flipCoin2[HH]}|awk '{print ($1/50)*100}'`
+ht=`echo ${flipCoin2[HT]}|awk '{print ($1/50)*100}'`
+th=`echo ${flipCoin2[TH]}|awk '{print ($1/50)*100}'`
+tt=`echo ${flipCoin2[TT]}|awk '{print ($1/50)*100}'`
+echo "Percentage of 'HT' : $ht%"
+echo "Percentage of 'HH' : $hh%"
+echo "Percentage of 'TT' : $tt%"
+echo "Percentage of 'TH' : $th%"
